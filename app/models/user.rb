7191ec294f8b8a5d089eac69072ab2473ce6d006
kuniyuki isaji <rrkrgft@gmail.com>
2023-02-14 08:23:26 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximam: 30 }
+  validates :email, presence: true, length: { maximam: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+
+  before_validation { email.downcase! }
+  #モデルにアドレスを小文字にする処理を入れる
 end
