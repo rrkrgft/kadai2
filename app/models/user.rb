d429d1b9daed4a5a1df1cb7e684dd5134f394774
kuniyuki isaji <rrkrgft@gmail.com>
2023-02-14 08:42:12 +0900

    userモデルの設定の終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximam: 30 }
-  validates :email, presence: true, length: { maximam: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :password, length: { minimum: 6 }
 
   before_validation { email.downcase! }
   #モデルにアドレスを小文字にする処理を入れる
+  has_secure_password
+  #パスワードをハッシュ化する。事前にgem bcryptを追加
+
 end
