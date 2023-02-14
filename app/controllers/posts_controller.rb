636a98df771998171f631c7bdb689ef4cdc4a5b6
kuniyuki isaji <rrkrgft@gmail.com>
2023-02-14 18:25:27 +0900

    postのviewを作成,登録作業まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,27 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :image)
   end
 end
