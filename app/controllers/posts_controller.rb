9c11d4d5bff04ce93583c5c54fe6be2472b47edf
kuniyuki isaji <rrkrgft@gmail.com>
2023-02-15 15:48:19 +0900

    編集機能、削除機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy ]
+
   def index
     @posts = Post.all
   end
@@ -18,15 +20,31 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "投稿を編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました"
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :text, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
